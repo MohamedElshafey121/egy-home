54d2aa97d4b4096c1c2917003d0da1fb32fe8bdd
unknown <mgmohamed11@gmail.com>
2021-12-14 22:08:37 +0200

    hello dashboard

--- backEnd/app.js
+++ backEnd/app.js
@@ -66,7 +66,13 @@ app.use("/uploads", express.static(path.join(path.resolve(), "/uploads")));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/dashboard-production/build")));
 
+  app.get("/dashboard", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "dashboard-production", "build", "index.html")
+    )
+  );
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
