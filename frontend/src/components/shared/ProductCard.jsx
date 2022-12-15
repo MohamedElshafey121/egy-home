6922e6f5a5e75fda91fe6e5c11afc4694f2a76ea
Gamal <mgmohamed11@gmail.com>
2022-12-15 03:31:38 +0200

    images update

--- frontend/src/components/shared/ProductCard.jsx
+++ frontend/src/components/shared/ProductCard.jsx
@@ -183,7 +183,9 @@ function ProductCard ( props ) {
             
             <div className="product-card__image product-image">
                 <Link to={url.product( product )} className="product-image__body" >
-                    <img className="product-image__img" src={`/uploads/imgs/products/${ selectedImage?selectedImage:product.photo }`} alt="" />
+                    {/* <img className="product-image__img" src={`/uploads/imgs/products/${ selectedImage ? selectedImage : product.photo }`} alt="" /> */}
+                    <img className="product-image__img" src={`/uploads/imgs/products/${ product.photo }`? ` /uploads/imgs/products/${ product.photo }` :` /uploads/product-16.jpg`} alt="" />
+                    
                 </Link>
             </div>
             {shapesShow&& images}
